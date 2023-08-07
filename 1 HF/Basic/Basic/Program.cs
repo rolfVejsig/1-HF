c9f82c30ef27ca74fe606f28da31ffe4093834a7
Rolf Lyn Vejsig Nielsen <115003279+rolfVejsig@users.noreply.github.com>
2023-08-07 08:56:29 +0200

    Add files via upload

---
+++ 1 HF/Basic/Basic/Program.cs
@@ -0,0 +1,127 @@
+﻿namespace Basic
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Add two numbers
+            //Given three numbers, write a method that adds two first ones and multiplies them by a third one.
+            static int addThreeNumbers(int num1, int num2, int num3)
+            {
+                return (num1 + num2) * num3;
+            }
+
+            Console.WriteLine(addThreeNumbers(2, 4, 5));
+
+            //Celsius to Fahrenheit
+            //Given a temperature in Celsius degrees, write a method that converts it to Fahrenheit degrees. Remember that temperature below -271.15°C (absolute zero) does not exist!
+
+            static string celsiusToFahrenheit(decimal celsius)
+            {
+
+                if (celsius < -271.15m)
+                {
+                    return "Temperature below absolute zero!";
+                }
+                else
+                {
+                    return (celsius * 9 / 5 + 32).ToString();
+                }
+            }
+
+            Console.WriteLine(celsiusToFahrenheit(0));
+            Console.WriteLine(celsiusToFahrenheit(100));
+            Console.WriteLine(celsiusToFahrenheit(-300));
+
+            //Elementary operations
+            //Given two integers, write a method that returns results of their elementary arithmetic operations: addition, substraction, multiplication, division. Remember that you can't divide any number by 0!
+
+            static decimal[] elementaryArithmetic(decimal num1, decimal num2)
+            {
+                decimal[] result = new decimal[4];
+                result[0] = num1 + num2;
+                result[1] = num1 - num2;
+                result[2] = num1 * num2;
+                if (num1 != 0 && num2 != 0)
+                {
+                    result[3] = num1 / num2;
+                }
+                else
+                {
+                    result[3] = 0;
+                }
+                return result;
+            }
+
+            decimal[] result = elementaryArithmetic(0, 5);
+            Console.WriteLine($"{result[0]}, {result[1]}, {result[2]}, {result[3]}");
+
+            //Is result the same
+            //Given two different arithmetic operations (addition, subtraction, multiplication, division), write a method that checks if they return the same result.
+
+            static bool isResultTheSame(int num1, int num2)
+            {
+                if (num1 == num2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Console.WriteLine(isResultTheSame(2 + 2, 2 * 2));
+            Console.WriteLine(isResultTheSame(9 / 3, 16 - 1));
+
+
+            //Modulo operations
+            //Given three integers, write a method that returns first number divided modulo by second one and these divided modulo by third one.
+            static int ModuloOperations(int num1, int num2, int num3)
+            {
+                return (num1 % num2) % num3;
+            }
+
+            Console.WriteLine(ModuloOperations(8, 5, 2));
+
+            //The cube of
+            //Given a number, write a method that returns its cube.
+            static decimal TheCubeOf(decimal num)
+            {
+                return num * num * num;
+            }
+
+            Console.WriteLine(TheCubeOf(2));
+            Console.WriteLine(TheCubeOf(-5.5m));
+
+            //Swap two numbers
+            //Given two integers, write a method that swaps them using temporary variable.
+            static string SwapTwonumbers(int num1, int num2)
+            {
+                string before = $"before num1 = {num1}, num2 = {num2}";
+                int temp = num1;
+                num1 = num2;
+                num2 = temp;
+                string after = $"; after num1 = {num1}, num2 = {num2}";
+
+                return before + after;
+
+            }
+            Console.WriteLine(SwapTwonumbers(87, 45));
+            Console.WriteLine(SwapTwonumbers(-13, 2));        
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ 1 HF/Conditional Statements/Conditional Statements/Program.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System;
+using System.Threading;
+
+namespace Conditional_Statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Absolute value
+            //Given an integer, write a method that returns its absolute value.
+
+            static int AbsoluteValue (int num1)
+            {
+                return Math.Abs(num1);
+            }
+
+            Console.WriteLine(AbsoluteValue(6832));
+            Console.WriteLine(AbsoluteValue(-392));
+
+            //Divisible by 2 or 3
+            //Given two integers, write a method that returns their multiplication if they are both divisible by 2 or 3, otherwise returns thier sum.
+            static int DivisibleBy2Or3(int num1, int num2)
+            {
+                if ((num1 % 2 == 0 || num1 % 3 == 0) && (num2 % 2 == 0 || num2 % 3 == 0))
+                {
+                    return num1 * num2;
+                }
+                else
+                {
+                    return num1 + num2;
+                }
+            }
+
+            Console.WriteLine(DivisibleBy2Or3(15, 30));
+            Console.WriteLine(DivisibleBy2Or3(2, 90));
+            Console.WriteLine(DivisibleBy2Or3(7, 12));
+
+            //If consists of uppercase letters
+            //Given a 3 characters long string, write a method that checks if it consists only of uppercase letters.
+
+            static bool IfConsistsOfUppercaseLetters(string str)
+            {
+                if (str.Length == 3)
+                {
+                    foreach (char c in str)
+                    {
+                        if (!Char.IsUpper(c))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Console.WriteLine(IfConsistsOfUppercaseLetters("xyz"));
+            Console.WriteLine(IfConsistsOfUppercaseLetters("DOG"));
+            Console.WriteLine(IfConsistsOfUppercaseLetters("L9#"));
+
+            //If greater than third one
+            //Given an array of 3 integers, write a method that checks if multiplication or sum of two first numbers is greater than third one excpetec output and input IfGreaterThanThirdOne([2, 7, 12]) == true.
+
+            static bool IfGreaterThanThirdOne(int[] arr)
+            {
+                if (arr.Length == 3)
+                {
+                    if (arr[0] * arr[1] > arr[2] || arr[0] + arr[1] > arr[2])
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                } 
+            }
+
+            Console.WriteLine(IfGreaterThanThirdOne(new int[] {2,7,12}));
+            Console.WriteLine(IfGreaterThanThirdOne(new int[] {-5,-8,50}));
+
+            //If number is even
+            //Given an integer, write a method that checks if it is even.
+
+            static bool IfNumberIsEven(int num1)
+            {
+                if (num1 % 2 == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Console.WriteLine(IfNumberIsEven(721));
+            Console.WriteLine(IfNumberIsEven(1248));
+
+            //If sorted ascending
+            //Given an array of three integers, write a method that checks if they are sorted in ascending order.
+            static bool IfSortedAscending(int[] arr)
+            {
+                for (int i = 1; i < arr.Length; i++)
+                {
+                    if (arr[i - 1] > arr[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            Console.WriteLine(IfSortedAscending(new int[] {3,7,10}));
+            Console.WriteLine(IfSortedAscending(new int[] {74,62,99}));
+
+            //Positive, negative or zero
+            //Given a number, write a method that checks if it is positive, negative or zero.
+
+            static string PositiveNegativeOrZero(decimal num1)
+            {
+                if (num1 > 0)
+                {
+                    return "positive";
+                }
+                else if (num1 < 0)
+                {
+                    return "negative";
+                }
+                else
+                {
+                    return "zero";
+                }
+            }
+
+            Console.WriteLine(PositiveNegativeOrZero(5.24m));
+            Console.WriteLine(PositiveNegativeOrZero(0));
+            Console.WriteLine(PositiveNegativeOrZero(-994.53m));
+
+            static bool IfYearIsLeap (int year)
+            {
+                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+                {
+                    return true;
+                }
+                else 
+                { 
+                    return false; 
+                }
+            }
+
+            Console.WriteLine(IfYearIsLeap(2016));
+            Console.WriteLine(IfYearIsLeap(2018));
+        }
+    }
+}
