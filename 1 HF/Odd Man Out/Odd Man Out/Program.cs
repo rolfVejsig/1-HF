d52c94be1247e427f5f16a732328ea3aaf350f16
rolfVejsig <rolfvejsig@gmail.com>
2023-08-10 10:20:17 +0200

    Dice

---
+++ 1 HF/Odd Man Out/Odd Man Out/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Odd_Man_Out
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OddManOut(new int[] {9,3,9,3,9,7,9,}));
+        }
+
+        static int OddManOut(int[] A)
+        {
+            Array.Sort(A);
+
+            for (int i = 0; i < A.Length; i += 2 )
+            {
+                if (i + 1 == A.Length || A[i] != A[i + 1])
+                {
+                    return A[i];
+                }
+            }
+
+            throw new ArgumentException("No odd man out");
+        }
+    }
+}
---
+++ 1 HF/Terningespil/Terningespil/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Terningespil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.Write("Number of dice: ");
+            int numDice = int.Parse(Console.ReadLine());
+
+            int throws = 0;
+            while (true)
+            {
+                throws++;
+                int[] diceRolls = new int[numDice];
+                for (int i = 0; i < numDice; i++)
+                {
+                    diceRolls[i] = random.Next(1, 7);
+                }
+
+                bool allSixes = true;
+                foreach (int result in diceRolls)
+                {
+                    if (result != 6)
+                    {
+                        allSixes = false;
+                        break;
+                    }
+                }
+
+                if (allSixes)
+                {
+                    Console.WriteLine($"It took {throws} throws");
+                    break;
+                }
+            }
+        }
+    }
+}
