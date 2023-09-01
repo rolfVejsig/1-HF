017573ea11470091fe28977f041f3a172b30cb73
rolfVejsig <rolfvejsig@gmail.com>
2023-09-01 09:02:23 +0200

    Penge

---
+++ 1 HF/Det lille pengeinstitut/Det lille pengeinstitut/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Det_lille_pengeinstitut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
