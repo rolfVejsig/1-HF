f741db967fb6a794efdc5cfd7f68ffaee66203bf
rolfVejsig <rolfvejsig@gmail.com>
2023-09-08 09:15:11 +0200

    PlukList

--- 1 HF/Plukliste/ClassLibrary1/Pluklist.cs
+++ 1 HF/Plukliste/ClassLibrary1/Pluklist.cs
@@ -48,11 +48,13 @@
 }
 
 /*
+ Æmdringer i forhold til den originale kode:
+
  Properties: Jeg har omdannet de offentlige felter til properties. Dette er mere i tråd med C#-best practice og tillader nemmere datamanipulation og -validering.
 
-Constructors: Jeg har tilføjet parameteriserede constructors for Pluklist og Item klasser, som gør det muligt at initialisere objekter med bestemte værdier.
+ Constructors: Jeg har tilføjet parameteriserede constructors for Pluklist og Item klasser, som gør det muligt at initialisere objekter med bestemte værdier.
 
-Initialisering af List: Jeg har initialiseret Lines listen direkte i property-definitionen, hvilket er mere i tråd med C# 6.0+ syntaks.
+ Initialisering af List: Jeg har initialiseret Lines listen direkte i property-definitionen, hvilket er mere i tråd med C# 6.0+ syntaks. 
 
-Tom Constructor: En tom constructor er tilføjet til både Pluklist og Item klasserne, hvilket er nyttigt, når objekter skal deserialiseres fra XML eller andre dataformater.
+ Tom Constructor: En tom constructor er tilføjet til både Pluklist og Item klasserne, hvilket er nyttigt, når objekter skal deserialiseres fra XML eller andre dataformater.
 */
